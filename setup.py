5d58181cd857d7e5da90e5e9207f6bec12a4add4
Jay Hombal <mh4ey@virginia.edu>
2021-11-02 07:51:55 -0400

    Initial Commit2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We aim to predict above ground biomass for forest trees',
+    author='Jay Hombal, Paul Hicks and Francisco Estrada',
+    license='MIT',
+)
